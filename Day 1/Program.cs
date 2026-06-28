9ce292984641d5b61f9d931292d6a06e1a18a1bf
subrat623 <subratsamal623@gmail.com>
2026-06-28 13:15:42 +0530

    Day 1: Variables and Data Types
    Completed C# basics including variables, data types, user input and console applications.

---
+++ Day 1/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+/*var var1 = "Hi Subrat";
+int a = 20;
+string b="hi buddy ";
+byte c= 10;
+char d='A';
+double e= 10.5;
+float f= 10.5f;
+decimal g = 10.5m;
+int test = 7 / 2;
+double test1 = 7 / 2.0; 
+Console.WriteLine(var1);
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(c);
+Console.WriteLine(d);
+Console.WriteLine(e);
+Console.WriteLine(f);
+Console.WriteLine(test);
+Console.WriteLine(test1);*/
+
+String name = "Subrat"; 
+int age= 25;    
+int height= 5;
+int weight= 60; 
+decimal salary= 10000.50m;
+Console.WriteLine(name);
+Console.WriteLine(age);
+Console.WriteLine(weight);
+Console.WriteLine(salary);
+
+Console.WriteLine("Name:");
+string name1 = Console.ReadLine();
+Console.WriteLine(name1);
+Console.WriteLine("Age:");
+int age1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(age1);
+Console.WriteLine("city:");
+string city1 = Console.ReadLine();
+Console.WriteLine(city1);
+
+
+
