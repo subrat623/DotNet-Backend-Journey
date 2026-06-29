9b07f144f0091dcf96e9051698d8ebf59250e68a
subrat623 <subratsamal623@gmail.com>
2026-06-29 09:58:35 +0530

    Day 2: Type Casting and Stack vs Heap
    Learned implicit and explicit type casting, TryParse, and basic Stack vs Heap concepts.

---
+++ Day02/Day02/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Day02;
+
+double a = 12345667;
+int b = (int)a;
+
+byte num= 255;
+int num2 = num;  // Implicit conversion from byte to int    
+Console.WriteLine($"Implicit conversion from byte to int: {num2}"); 
+
+// String to int conversion
+
+String str = "123456";
+int result = 0;
+if (int.TryParse(str, out result))
+{
+    Console.WriteLine($"Conversion successful: {result}");
+}
+else
+{
+    Console.WriteLine("Conversion failed.");
+}
+
+StackHeapExample stackHeapExample = new StackHeapExample();
+stackHeapExample.AddFive(10);
+
+
---
+++ Day02/Day02/StackHeapExample.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day02
+{
+    public class StackHeapExample
+    {
+        public int AddFive(int number)
+        {
+            return number + 5;
+        }
+    }
+}
