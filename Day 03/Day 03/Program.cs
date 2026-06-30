f8c0926e8ed7bdf5f21996a7295925818219ce75
subrat623 <subratsamal623@gmail.com>
2026-06-30 09:51:01 +0530

    Day 3: Implemented C# array fundamentals
    - Implemented array declaration and initialization
    - Practiced element access and modification
    - Explored built-in array methods (Sort, Average)
    - Worked with array indexing and Length property
    - Added coding exercises for array fundamentals

---
+++ Day 03/Day 03/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//fixed array
+
+
+int[] vs= new int[5]; 
+vs[0] = 1; 
+vs[1] = 2; 
+vs[2] = 3; 
+vs[3] = 4; 
+vs[4] = 5;
+//Console.WriteLine(vs);
+
+//dynamic array
+int [] vs2 = new int[] {5,6,7,8,2,3};
+Array.Sort(vs2);
+double vs3 = vs2.Average();
+double vs4 = vs2.Length- 1;
+Console.WriteLine(vs3);
+//updated index 2 with 100
+vs2[2] = 100;
+
+
+Console.WriteLine(" first number:" + vs2[0]);
+Console.WriteLine(" last number:" + vs2[(int)vs4-1]);
+
+
