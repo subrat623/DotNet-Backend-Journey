aeae755bce579601defedc0d82fd0a7c6f54b39d
subrat623 <subratsamal623@gmail.com>
2026-07-06 09:51:27 +0530

    Day 5: Added C# string manipulation and immutability concepts
    - Implemented string handling examples using C#
    - Practiced string literals, verbatim strings, and escape sequences
    - Used Trim(), ToUpper(), and Length for string manipulation
    - Explored method chaining with Console.ReadLine().Trim()
    - Learned why strings are immutable and how string methods return new objects
    - Reinforced understanding of methods, properties, return types, and object references

---
+++ Day05/Day05/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string example = "hello subrat";
+//literal
+string filePath = "C:\\Users\\Subrat\\Documents\\example.txt";
+//verbatim
+string verbatimFilePath = @"C:\Users\Subrat\Documents\example.txt";
+
+Console.WriteLine("hello!please enter your first name and last name ");
+string firstName = Console.ReadLine();
+string lastName = Console.ReadLine();
+
+Console.WriteLine($"welcome {firstName.ToUpper()} {lastName.ToUpper()}");
+Console.WriteLine($"{firstName.Length}{lastName.Length}");
+Console.WriteLine(firstName.Length);
