742d7976031e705030050bb4bf16f674b1976508
subrat623 <subratsamal623@gmail.com>
2026-07-07 10:25:29 +0530

    Day 6: Added C# List<T> collection and collection manipulation
    - Implemented dynamic collections using List<T>
    - Practiced collection operations including Add(), Remove(), RemoveAt(), and Count
    - Compared arrays with List<T> and understood when to use each
    - Learned foreach iteration for processing collection elements
    - Explored list indexing behavior after element removal
    - Improved understanding of runtime exceptions and collection APIs
    - Reinforced concepts of methods, properties, return types, and object-oriented thinking

---
+++ Day06/Day06/Part.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day06
+{
+   public class Part
+    {
+        public string partName { get; set; }
+        public int partId { get; set; }
+    }
+
+}
---
+++ Day06/Day06/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+List<string> lst = new List<string>();
+lst.Add("ironman");
+lst.Add("captain marvel");
+lst.Add("Black panther");
+
+//adding new item in array 
+Console.WriteLine("enter the favorite movie");
+lst.Add(Console.ReadLine());
+
+
+//int count = lst.Count;
+//remove
+lst.Remove("captain marvel");
+foreach (string prt in lst)
+{
+    Console.WriteLine(prt);
+}
+;
+Console.WriteLine(lst[1]);
+Console.WriteLine("total count="+ lst.Count);
+
+
