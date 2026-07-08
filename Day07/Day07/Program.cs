bfb8d140927ec0cc7b87965e4dd62cd29775a7b0
subrat623 <subratsamal623@gmail.com>
2026-07-08 10:02:49 +0530

    Day 7: Implemented C# conditional statements
    - Practiced decision making using if, else if, and else statements
    - Implemented console applications for voting eligibility, grade calculation, password validation, and driving eligibility
    - Explored comparison and logical operators (==, >=, &&, ||)
    - Learned that if conditions must evaluate to a boolean value
    - Strengthened understanding of conditional flow and program execution

---
+++ Day07/Day07/Program.cs
@@ -0,0 +1,53 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter your age: ");
+int age = int.Parse(Console.ReadLine());
+if (age>=18)
+{     Console.WriteLine("eligible to vote");
+}
+else
+{
+    Console.WriteLine("Not eligible to vote");
+}
+
+Console.WriteLine("Enter your Mark: ");
+int mark = int.Parse(Console.ReadLine());
+if (mark>=90)
+{
+    Console.WriteLine("Grade A");
+}
+else if (mark>=80)
+{
+    Console.WriteLine("Grade B");
+}
+else if (mark>=70)
+{
+    Console.WriteLine("Grade C");
+}
+else
+{
+    Console.WriteLine("Grade D");
+}
+
+Console.WriteLine("Enter your password: ");
+string password = Console.ReadLine();
+if(password=="admin123")
+{
+    Console.WriteLine("login successful");
+}
+else
+{
+    Console.WriteLine("Invalid password");
+}
+
+Console.WriteLine("do you have driving license: ");
+string license = Console.ReadLine();
+Console.WriteLine("enter your age: ");
+int drivingage = int.Parse(Console.ReadLine());
+if(license=="yes" && drivingage>=18)
+{
+    Console.WriteLine("eligible to drive");
+}
+else
+{
+    Console.WriteLine("Not eligible to drive");
+}
