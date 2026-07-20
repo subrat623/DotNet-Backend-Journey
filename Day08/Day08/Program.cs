5980d8b6b3e9164e0fa6031993acf8ab1eda8aee
subrat623 <subratsamal623@gmail.com>
2026-07-20 09:25:48 +0530

    Day 8: Implemented switch statements and for loops
    - Practiced switch statements with integer values, enums, and pattern matching
    - Implemented calculator and measurement classification using switch
    - Learned the purpose of case, break, and default
    - Practiced for loops with incrementing and decrementing counters
    - Built programs for number printing and multiplication tables
    - Strengthened loop tracing and output prediction skills

---
+++ Day08/Day08/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Day08;
+
+Console.WriteLine("Enter the measurement : ");
+double measurement = Convert.ToDouble(Console.ReadLine());
+switch (measurement)
+{
+    case <0.0:
+        Console.WriteLine("Invalid measurement");
+        break;
+        case >= 0.0 and < 10.0:
+            Console.WriteLine("Measurement is less than 10");
+            break;
+        case >= 10.0 and < 20.0:
+            Console.WriteLine("Measurement is between 10 and 20");
+            break;
+        case double.NaN:
+            Console.WriteLine("Measurement is not a number");
+            break;
+        default:
+            Console.WriteLine("Measurement is greater than or equal to 20");
+            break;
+}
+
+State state = State.active;
+switch(state)
+{
+    case State.active:
+        Console.WriteLine("State is active");
+        break;
+    case State.inactive:
+        Console.WriteLine("State is inactive");
+        break;
+    default:
+        Console.WriteLine("Unknown state");
+        break;
+}
+
+var fibNumbers = new List<int> { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34 };
+foreach(var number in fibNumbers)
+{
+   Console.WriteLine($"Fibonacci number: {number}");
+}
---
+++ Day08/Day08/State.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day08
+{
+    public enum State
+    {
+        active=1,
+                inactive= 0
+    }
+}
