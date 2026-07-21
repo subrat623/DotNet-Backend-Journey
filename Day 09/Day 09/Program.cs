bca4390dbca8983dab6130c33774f2d518b92b3a
subrat623 <subratsamal623@gmail.com>
2026-07-21 09:23:56 +0530

    Day DAY 09: Implemented C# Exception Handling
    - Learned the difference between compile-time and runtime errors
    - Implemented exception handling using try, catch, and finally blocks
    - Practiced handling DivideByZeroException and FormatException scenarios
    - Explored common exception types including NullReferenceException, IndexOutOfRangeException, and FileNotFoundException
    - Learned how to create and throw custom exceptions using the throw keyword
    - Understood the difference between throw and throw ex and why preserving the original stack trace is important
    - Built console applications to handle runtime exceptions gracefully instead of crashing
    - Practiced interview-style questions and real-world exception handling scenarios

---
+++ Day 09/Day 09/Program.cs
@@ -0,0 +1,54 @@
+﻿// See https://aka.ms/new-console-template for more information
+try
+{
+    Console.WriteLine("enter the first number:");
+    int a = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("enter the Second number:");
+    int b = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("The Division of the two numbers is: " + (a / b));
+
+}
+catch
+{
+    Console.WriteLine("Error: Division by zero is not allowed.");
+}
+finally
+{
+    Console.WriteLine("Press any key to exit...");
+
+}
+
+
+
+ static void checkage(int age)
+{
+    if (age < 18)
+    {
+        throw new Exception("Age must be at least 18.");
+    }
+    else
+    {
+        Console.WriteLine("Age is valid.");
+    }
+}
+
+try { 
+    checkage(15); // This will throw an exception
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+
+
+
+
+
+
+
+
+
+
+
