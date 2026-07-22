f1a2ec86d3ff6fd40c7ef4bf7cd0aae865eeaba7
subrat623 <subratsamal623@gmail.com>
2026-07-22 09:42:59 +0530

    Day 10: Learned LINQ Basics - Where() and Select()
    - Started learning LINQ (Language Integrated Query) fundamentals
    - Understood the purpose and advantages of LINQ over traditional foreach loops
    - Learned how to filter collections using the Where() method
    - Practiced filtering data using numeric conditions and string methods like StartsWith()
    - Learned that Where() returns a filtered collection without modifying the original source collection
    - Explored the Select() method to project specific properties from objects
    - Implemented LINQ queries to retrieve employee IDs, names, and filtered results
    - Understood the concept of method chaining using Where() followed by Select()
    - Practiced interview-oriented questions on LINQ execution flow, filtering, and projection
    - Learned the difference between filtering data (Where) and selecting data (Select)

---
+++ Day10/Day10/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day10
+{
+    class Employee
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public decimal Salary { get; set; }
+
+        public bool IsActive { get; set; }
+    }
+
+}
---
+++ Day10/Day10/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Day10;
+
+List<Employee> employees = new()
+{
+    new Employee{ Id=1, Name="Subrat", Age=25, Salary=50000, IsActive=true},
+    new Employee{ Id=2, Name="Rahul", Age=30, Salary=70000, IsActive=true},
+    new Employee{ Id=3, Name="Amit", Age=22, Salary=30000, IsActive=false},
+    new Employee{ Id=4, Name="Sourav", Age=28, Salary=90000, IsActive=true},
+    new Employee{ Id=5, Name="Rakesh", Age=24, Salary=40000, IsActive=false},
+};
+
+List<int> numbers = new()
+{
+    10,20,30,40,50
+};
+
+foreach(var num in numbers)
+{
+    if (num > 30) 
+    { Console.WriteLine(num);
+    }
+};
+List<string> names = new()
+{
+    "Subrat",
+    "Rahul",
+    "Amit",
+    "Sourav",
+    "Rakesh"
+};
+var result= names.Where(n => n.StartsWith("S"));
+foreach(var name in result)
+{
+    Console.WriteLine(name);
+}
+
+var id= employees.Select(employees => employees.Id);
+foreach(var i in id)
+{
+    Console.WriteLine(i);
+}
