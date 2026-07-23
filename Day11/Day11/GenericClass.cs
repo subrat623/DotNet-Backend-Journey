048008ca7cf60045275acdadd02d9bcb25bd1766
subrat623 <subratsamal623@gmail.com>
2026-07-23 09:43:56 +0530

    Day 11: Learned C# Generics
    - Learned the fundamentals of C# Generics and why they were introduced
    - Understood the limitations of using object compared to Generics
    - Implemented generic methods using type parameters (<T>)
    - Created and used custom generic classes
    - Learned how generic type inference works in C#
    - Explored the advantages of Generics including type safety, code reusability, and compile-time checking
    - Understood why Generics eliminate the need for explicit casting
    - Compared Generics with object and discussed boxing/unboxing concepts
    - Solved interview-oriented coding exercises and reasoning-based questions
    - Explored real-world uses of Generics in List<T>, Dictionary<TKey, TValue>, Task<T>, and IEnumerable<T>

---
+++ Day11/Day11/GenericClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day11
+{
+    public class GenericClass<T>
+    {
+        public T Data;
+        public GenericClass(T data) {
+            Data = data;
+        }
+
+    }
+}
---
+++ Day11/Day11/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Day11;
+
+var myClass = new GenericClass<string>("hello");
+
+void printData<T>(T input) {
+    Console.WriteLine(input);
+}
+printData("hi Subrat");
